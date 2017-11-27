73fdaca92b237711844056e82b146d30e5875dff
Nikita Aplin <aplinxy9plin@icloud.com>
2017-11-27 17:20:09 +0700

    add type

--- server.php
+++ server.php
@@ -1,33 +1,32 @@
 <?php
-	if(isset($_POST['type'])){
-		$mysqli = new mysqli("localhost","top4ek","Top4ek2281337!","shop");
-		$mysqli->set_charset("utf8");
-		switch ($_POST['type']) {
-			case 'add':
-				$barcode = $_POST["barcode"];
-				$sql = $mysqli->query("SELECT name, price FROM `products` WHERE `barcode` = '$barcode'");
-				if($sql->num_rows > 0) {
-				    while($row = $sql->fetch_assoc()) {
-				    	$json_array = [
-				            "type" => "response",
-				            "products" => [
-				                [
-				                    "name" => $row['name'],
-				                    "price" => $row['price']
-				                ]
-				            ]
-				        ]; 
-						$body = json_encode($json_array);
-						echo $body;
-				    	//var_dump(json_decode($body));
-				    }
-				}
-				break;
-			case 'buy':
-				
-				break;
-			default:
-				# code...
-				break;
-		}
+	$data = json_decode(file_get_contents("php://input"));
+	//echo $data->type;
+	switch ($data->type) {
+		case 'add':
+			$mysqli = new mysqli("localhost","top4ek","Top4ek2281337!","shop");
+			$mysqli->set_charset("utf8");
+			$barcode = $data->barcode;
+			$sql = $mysqli->query("SELECT name, price FROM `products` WHERE `barcode` = '$barcode'");
+			if($sql->num_rows > 0) {
+			    while($row = $sql->fetch_assoc()) {
+			    	$json_array = [
+			            "type" => "response",
+			            "products" => [
+			                [
+			                    "name" => $row['name'],
+			                    "price" => $row['price']
+			                ]
+			            ]
+			        ]; 
+			        $body = json_encode($json_array, JSON_UNESCAPED_UNICODE);
+					echo $body;
+			    }
+			}
+			break;
+		case 'buy':
+			
+			break;
+		default:
+			# code...
+			break;
 	}
