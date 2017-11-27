a24e77b28aa5f9e2d61893f5d20ef1a5bdaf0548
Nikita Aplin <aplinxy9plin@icloud.com>
2017-11-27 13:27:19 +0700

    Working

--- server.php
+++ server.php
@@ -1,8 +1,21 @@
 <?php
 	if(isset($_POST['type'])){
+		$mysqli = new mysqli("localhost","top4ek","Top4ek2281337!","shop");
+		$mysqli->set_charset("utf8");
 		switch ($_POST['type']) {
 			case 'add':
-				
+				$sql = "SELECT name FROM products";
+				$result = $mysqli->query($sql);
+
+				if ($result->num_rows > 0) {
+				    // output data of each row
+				    while($row = $result->fetch_assoc()) {
+				        echo $row['name'];echo "<br>";
+				    }
+				} else {
+				    echo "0 results";
+				}
+				//var_dump($sql);
 				break;
 			case 'buy':
 				
